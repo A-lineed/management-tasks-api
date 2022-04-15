4179af371f9d739953a9dbf09071d397dd4afa49
A-lineed <alineedvania2018@gmail.com>
2022-04-15 15:44:32 -0300

    Implementação do create e update.

--- gestao-tarefas-api/src/main/java/com/gestao/tarefas/service/TarefaService.java
+++ gestao-tarefas-api/src/main/java/com/gestao/tarefas/service/TarefaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gestao.tarefas.domain.Tarefa;
+import com.gestao.tarefas.dtos.TarefaDTO;
 import com.gestao.tarefas.exceptions.ObjectNotFoundException;
 import com.gestao.tarefas.repositories.TarefaRepository;
 
@@ -24,6 +25,20 @@ public class TarefaService {
 	
 	public List<Tarefa> findAll(){
 		return repository.findAll();
+	}
+	
+	public Tarefa create(Tarefa obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Tarefa update(Integer id, TarefaDTO objDto) {
+		Tarefa obj = findById(id);
+		obj.setDeadline(objDto.getDeadline());
+		obj.setDescricao(objDto.getDescricao());
+		obj.setResponsavel(objDto.getResponsavel());
+		obj.setTitulo(objDto.getTitulo());
+		return repository.save(obj);
 	}
 
 }
